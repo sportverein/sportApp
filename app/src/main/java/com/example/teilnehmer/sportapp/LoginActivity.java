73e23641c95ee2b12047d9f80bbbaec05009a86f
sportVerein <seb@spvgg-ramspau.de>
2017-12-13 20:18:38 +0100

    gson

--- app/src/main/java/com/example/teilnehmer/sportapp/LoginActivity.java
+++ app/src/main/java/com/example/teilnehmer/sportapp/LoginActivity.java
@@ -19,6 +19,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -309,8 +310,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // TODO: attempt authentication against a network service.
             LoginRequest lr = new LoginRequest(mHost, Integer.parseInt(mStation));
             Gson gson = new Gson();
-            String json = gson.toJson(lr, LoginRequest.class);
-            Toast.makeText(getBaseContext(),json, Toast.LENGTH_LONG).show();
+            String json = gson.toJson(lr);
+            Log.e("json", json);
 
 
             try {
