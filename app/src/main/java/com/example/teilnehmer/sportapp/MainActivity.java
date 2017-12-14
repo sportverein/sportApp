6dcc3f824be1691af90d369bc8e95267c1370a5e
sportverein <seb@spvgg-ramspau.de>
2017-12-14 14:34:25 +0100

    RefreshSchoolclasses

--- app/src/main/java/com/example/teilnehmer/sportapp/MainActivity.java
+++ app/src/main/java/com/example/teilnehmer/sportapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.teilnehmer.sportapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ExpandableListView;
 
 import java.util.ArrayList;
@@ -21,10 +22,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        view_schoolclasses = (Spinner) findViewById(R.id.schoolclasses);
+        view_schoolclasses = (Spinner) findViewById(R.id.s_schoolclasses);
     }
 
     public void RefreshSchoolclasses(){
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, schoolclasses);
+        view_schoolclasses.setAdapter(adapter);
+        for(String s: schoolclasses){
+            adapter.add(s);
+            adapter.notifyDataSetChanged();
+        }
 
     }
 }
