6d5869023fb1a13474a33f5602318dfd1f77d7db
sportverein <seb@spvgg-ramspau.de>
2017-12-14 14:22:43 +0100

    Senddata Task

--- app/src/main/java/com/example/teilnehmer/sportapp/MainActivity.java
+++ app/src/main/java/com/example/teilnehmer/sportapp/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.teilnehmer.sportapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ExpandableListView view_schoolclasses;
+    private ArrayList<String> schoolclasses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        view_schoolclasses = (ExpandableListView) findViewById(R.id.schoolclasses);
+    }
+
+    public void RefreshSchoolclasses(){
+        view_schoolclasses.ad
     }
 }
